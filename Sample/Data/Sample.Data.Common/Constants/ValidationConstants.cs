d2a7ad09ec118daf0f3816a2f945d8fcb36b873d
InKolev <in.kolev.b@gmail.com>
2015-11-20 23:24:02 +0200

    Created Data structure.

---
+++ Sample/Data/Sample.Data.Common/Constants/ValidationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Sample.Data.Common.Constants
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ValidationConstants
+    {
+    }
+}
